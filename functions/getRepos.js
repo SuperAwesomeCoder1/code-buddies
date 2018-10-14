dabcb3d5e685a098f172e4c8bd88b041722493a2
Nathan <nathan.2002.rogers@gmail.com>
2018-10-13 21:57:18 -0400

    Added active people in the past week

---
+++ functions/getRepos.js
@@ -0,0 +1,49 @@
+const request = require("request");
+const axios = require("axios");
+const secret = "342361db8a1cbfedd55cf9e31b46d47111c94612";
+const oauthid = "526506a7c2a66d90c2dc";
+const endURL = "?client_id=" + oauthid + "&client_secret=" + secret;
+let githubLogin = "";
+exports.getUserSkills = async githubUserName => {
+  let url =
+    "https://api.github.com/users/" + githubUserName + "/repos" + endURL;
+  githubLogin = githubUserName;
+  let userData = await axios.get(url);
+  let commits = getCommits(userData.data);
+  let skills = await getWeightedSkills(commits);
+  return skills;
+};
+//returns array of commit api urls
+function getCommits(userReposBody) {
+  //  console.log('param body', userReposBody);
+  let commits = [];
+  for (let repo of userReposBody) {
+    let { commits_url } = repo;
+    commits_url = commits_url.substring(0, commits_url.length - 6);
+    commits.push({
+      url: commits_url + endURL,
+      language: repo.language
+    });
+  }
+  return commits;
+}
+
+async function getWeightedSkills(commitMappings) {
+  let skills = {};
+  for (let commitMapping of commitMappings) {
+    try {
+      let res = await axios.get(commitMapping.url);
+      for (let commit of res.data) {
+        if (commit.commit.author.name === githubLogin) {
+          if (!skills[commitMapping.language]) {
+            skills[commitMapping.language] = 0;
+          }
+          skills[commitMapping.language]++;
+        }
+      }
+    } catch (err) {
+      console.log(err.message);
+    }
+  }
+  return skills;
+}
--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,13 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
+const getUserSkills = require("./getRepos");
+const axios = require("axios");
+
+// For Rate Limiting
+const secret = "ea10485ed2eb7370c3704fc34d4986c502791726";
+const oauthid = "1f2826df09ac7cff9bd6";
+const endURL = "?client_id=" + oauthid + "&client_secret=" + secret;
 
 // Use cors
 app.use(cors({ origin: true }));
@@ -17,45 +24,64 @@ app.get("/github", (req, res) => {
 
 app.post("/accountInfo", (req, res) => {
   var github = req.body.github;
-  console.log(github);
   res.send(getGithub(github));
+  getPotentialUsers("ann+arbor");
 });
 
 function getGithub(github) {
+  getUserSkills
+    .getUserSkills(github)
+    .then(res => {
+      return res;
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+  return "hi";
+}
+
+async function getPotentialUsers(location) {
   var options = {
-    url: "https://api.github.com/users/" + github + "/repos",
+    url:
+      "https://api.github.com/search/users?q=location:" +
+      location +
+      "&per_page=10" +
+      endURL,
     headers: {
       "User-Agent": "code-buddies"
     }
   };
 
-  request(options, (error, response, body) => {
-    console.log("error:", error);
-    console.log("statusCode:", response && response.statusCode);
-    console.log("body:", body);
-    return body;
+  request(options, async (error, response, body) => {
+    var people = JSON.parse(body).items;
+    var potentialPeople = [];
+    for (let person of people) {
+      var temp = await getLastlog(person.login);
+      if (temp != "") {
+        potentialPeople.push(temp);
+      }
+    }
+    console.log(potentialPeople);
   });
+}
+
+async function getLastlog(github) {
+  var inPastWeek = "";
+  await axios
+    .get("https://api.github.com/users/" + github + endURL)
+    .then(lastLog => {
+      lastLog = lastLog.data;
+      var activity = new Date(lastLog.updated_at);
+      var now = new Date();
+      if (
+        activity.getFullYear() == now.getFullYear() &&
+        activity.getMonth() == now.getMonth() &&
+        activity.getDate() >= now.getDate() - 7
+      ) {
+        inPastWeek = lastLog.login;
+      }
+    });
+  return inPastWeek;
 }
 
 exports.findPartners = functions.https.onRequest(app);
-
-/*console.log(req);
-var linkedin = req.query.linkedin;
-var github = req.query.github;
-response.send(getGithub(github));
-function getGithub(github) {
-  var options = {
-    url: "https://api.github.com/users/" + github + "/repos",
-    headers: {
-      "User-Agent": "code-buddies"
-    }
-  };
-
-  request(options, (error, response, body) => {
-    console.log("error:", error);
-    console.log("statusCode:", response && response.statusCode);
-    console.log("body:", body);
-    return body;
-  });
-}
-*/
